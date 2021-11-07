e805bfbb8c87e81c59e9837829ef2338e96e9d87
Shawn Kelley <90985377+shawnkelleyart@users.noreply.github.com>
2021-11-06 19:30:14 -0600

    full functionality

--- app.js
+++ app.js
@@ -1,7 +1,56 @@
 const express = require("express");
-
 const app = express();
+const data = require("./projects.json");
 
+//sets up pug
+app.set("view engine", "pug");
+
+//serves static files to be read by user's browser
+app.use("/static", express.static("public"));
+
+//------------------------------------------------------------------
+
+//index
+app.get("/", (req, res) => {
+  res.render("index");
+  const projectsarr = data.projects;
+  res.locals.projects = projectsarr;
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/projects", (req, res) => {
+  res.locals.projects = data.projects;
+  res.locals.pieces = data.music;
+  res.render("projects");
+});
+
+app.get("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const project = data.projects[id];
+  res.locals.name = project.project_name;
+  res.locals.techs = project.technologies;
+  res.locals.desc = project.description;
+  res.locals.tag = project.tag;
+  res.locals.live = project.live_link;
+  res.locals.github = project.github_link;
+  res.render("project");
+});
+
+app.get("/music/:id", (req, res) => {
+  const { id } = req.params;
+  const project = data.music[id];
+  res.locals.title = project.title;
+  res.locals.instrum = project.instrumentation;
+  res.locals.duration = project.duration;
+  res.locals.description = project.description;
+  res.locals.video = project.video_link;
+  res.render("piece");
+});
+
+//server
 const route = 3000;
 app.listen(route, () => {
   console.log(`The application is running on localhost:${route}.`);
---
+++ public/js/script.js
@@ -0,0 +1,49 @@
+const ham = document.querySelector("#ham");
+const hamLI = ham.querySelectorAll("li");
+const menu = document.querySelector("#menu");
+const menuLI = document.querySelectorAll(".menu-li");
+
+ham.addEventListener("click", () => hamHandler());
+
+function hamHandler() {
+  if (menu.style.right !== "0px") {
+    menuIn();
+  } else {
+    menuOut();
+  }
+}
+
+function menuIn() {
+  right0(menu);
+  hamX();
+}
+
+function menuOut() {
+  right2000(menu);
+  hamE();
+}
+
+function right0(element) {
+  element.style.right = "0";
+}
+
+function right2000(element) {
+  element.style.right = "2000px";
+}
+
+function hamE() {
+  for (let i = 0; i < hamLI.length; i++) {
+    hamLI[i].style.background = "black";
+    hamLI[i].style.transform = "";
+    hamLI[i].style.top = "";
+    hamLI[i].style.bottom = "";
+  }
+}
+
+function hamX() {
+  hamLI[0].style.transform = "rotate(45deg)";
+  hamLI[0].style.top = "14px";
+  hamLI[1].style.background = "transparent";
+  hamLI[2].style.transform = "rotate(-45deg)";
+  hamLI[2].style.bottom = "14px";
+}
